025526b4ed6fd1533a8a5ff4806833b2ed541f3d
Ivkachaa <marinkovicivana@hotmail.com>
2017-02-23 15:59:27 +0100

    navigacija

--- restaurant-Ivana/public_html/js/main.js
+++ restaurant-Ivana/public_html/js/main.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
      $('.search').click(function(){
         $('.form-group').toggle(); 
      });
+   $('.bxslider').bxSlider({
+  mode: 'fade',
+  captions: true
+});
+$('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
     
     
 });
