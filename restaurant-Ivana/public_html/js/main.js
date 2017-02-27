b2eb2d8b2bd7cc04e6c50180cec8b6eb271dc3ef
Ivkachaa <marinkovicivana@hotmail.com>
2017-02-27 21:54:22 +0100

    main-prva sekcija-zavrsen slajder

--- restaurant-Ivana/public_html/js/main.js
+++ restaurant-Ivana/public_html/js/main.js
@@ -1,16 +1,35 @@
-$(document).ready(function(){
-     $('.search').click(function(){
-        $('.form-group').toggle(); 
-     });
-   $('.bxslider').bxSlider({
-  mode: 'fade',
-  captions: true
-});
-$('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-    
-    
+$(document).ready(function () {
+    $('.search').click(function () {
+        $('.form-group').toggle();
+    });
+
+//    $('#myTabs a').click(function (e) {
+//        e.preventDefault()
+//        $(this).tab('show')
+//    })
+
+
+  // The slider being synced must be initialized first
+$('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: false,
+    itemWidth: 50,
+    itemMargin: 0,
+    direction: "vertical",
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation:  "slide",
+    controlNav: false,
+    animationLoop: true,
+    slideshow: false,
+    sync: "#carousel"
+  });
+
+
+
 });
 
