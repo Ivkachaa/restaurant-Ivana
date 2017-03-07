47a71304545216ba58317d8417d4169e3928c135
Ivkachaa <marinkovicivana@hotmail.com>
2017-03-07 16:04:58 +0100

    na casu about us

--- restaurant-Ivana/public_html/js/main.js
+++ restaurant-Ivana/public_html/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    $('.search').click(function () {
+    $('.search').click(function (e) {
+        e.preventDefault();
         $('.form-group').toggle();
     });
 
